cf394a8087b2a4aebdaf14367feb797e5973287e
den0206 <s.yuuki0206@gmail.com>
2024-12-08 13:33:08 +0900

    SearchRepositoryResponse Model テスト生成

---
+++ test/src/domain/entity/github/repository/search/response_test.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:yumemi_flutter_test/src/domain/entity/github/repository/search/response.dart';
+
+import 'mock_data.dart';
+
+void main() {
+  group('/search/repositories モック変換テスト', () {
+    final mockData = json.decode(MockData.searchResponse);
+    test('fromJson', () {
+      final result = SearchRepositoryResponse.fromJson(mockData);
+
+      expect(result.totalCount, 83181);
+      expect(result.items.length, 30);
+
+      final item = result.items.first;
+
+      // 文字列変換が正常か
+      expect(item.fullName, 'openai/openai-cookbook');
+      expect(item.description, 'Examples and guides for using the OpenAI API');
+      expect(item.language, 'MDX');
+      expect(item.htmlUrl, 'https://github.com/openai/openai-cookbook');
+
+      // 数値変換が正常か
+      expect(item.stargazersCount, 60510);
+      expect(item.watchersCount, 60510);
+      expect(item.forksCount, 9627);
+      expect(item.openIssuesCount, 61);
+
+      // ネストされたObject の変換が正常か
+      const owner = OwnerSchema(
+        login: 'openai',
+        avatarUrl: 'https://avatars.githubusercontent.com/u/14957082?v=4',
+      );
+
+      expect(item.owner, owner);
+    });
+  });
+}
