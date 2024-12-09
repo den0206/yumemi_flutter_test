5d134e1232687179f29eeaa3d168fe5d6efe704f
den0206 <s.yuuki0206@gmail.com>
2024-12-09 21:13:57 +0900

    【Test】GithubFactory の実装

---
+++ test/src/domain/factory/github_factory.dart
@@ -0,0 +1,62 @@
+import 'package:yumemi_flutter_test/src/domain/entity/github/repository/search/response.dart';
+
+import 'base_factory.dart';
+
+final class SearchRepositoryResponseFactory
+    extends BaseFactory<SearchRepositoryResponse> {
+  /// 単体のフェイクモデルの生成
+  @override
+  SearchRepositoryResponse generateFake() {
+    return SearchRepositoryResponse(
+      totalCount: rInteger,
+      items: RepositoryFactory().generateFakeList(),
+    );
+  }
+
+  /// 配列のフェイクモデルの作成
+  @override
+  List<SearchRepositoryResponse> generateFakeList({int? length}) {
+    length ??= faker.randomGenerator.integer(10);
+    return List.generate(length, (_) => generateFake());
+  }
+}
+
+final class RepositoryFactory extends BaseFactory<RepositorySchema> {
+  /// 単体のフェイクモデルの生成
+  @override
+  RepositorySchema generateFake() {
+    return RepositorySchema(
+      fullName: rString,
+      owner: OwnerFactory().generateFake(),
+      description: rWord,
+      stargazersCount: rInteger,
+      watchersCount: rInteger,
+      openIssuesCount: rInteger,
+      forksCount: rInteger,
+      language: rWord,
+      htmlUrl: rUrl,
+    );
+  }
+
+  /// 配列のフェイクモデルの作成
+  @override
+  List<RepositorySchema> generateFakeList({int? length}) {
+    length ??= faker.randomGenerator.integer(10);
+    return List.generate(length, (_) => generateFake());
+  }
+}
+
+final class OwnerFactory extends BaseFactory<OwnerSchema> {
+  /// 単体のフェイクモデルの生成
+  @override
+  OwnerSchema generateFake() {
+    return OwnerSchema(login: rName, avatarUrl: rUrl);
+  }
+
+  /// 配列のフェイクモデルの作成
+  @override
+  List<OwnerSchema> generateFakeList({int? length}) {
+    length ??= faker.randomGenerator.integer(10);
+    return List.generate(length, (_) => generateFake());
+  }
+}
