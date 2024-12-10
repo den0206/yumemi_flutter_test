33966a27ab86a5fe94e3f9ca62e52f4ff75d40f5
den0206 <s.yuuki0206@gmail.com>
2024-12-10 21:34:45 +0900

    【機能】ページネーション機能実装

--- lib/src/presentation/pages/github_search/components/repository_list.dart
+++ lib/src/presentation/pages/github_search/components/repository_list.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 import 'package:yumemi_flutter_test/src/core/extension/context.dart';
+import 'package:yumemi_flutter_test/src/domain/entity/github/repository/search/response.dart';
 import 'package:yumemi_flutter_test/src/presentation/pages/github_detail/github_detail_page.dart';
 import 'package:yumemi_flutter_test/src/presentation/pages/github_search/github_search_notifier.dart';
+import 'package:yumemi_flutter_test/src/presentation/pages/github_search/github_search_state.dart';
 
 final class RepositoryList extends ConsumerWidget {
   const RepositoryList({super.key});
@@ -11,15 +14,14 @@ final class RepositoryList extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(githubSearchNotifierProvider);
 
-    // ローディング中
-    if (state.isLoading) {
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
-    }
-
     // 検索結果なし
     if (state.repositories.isEmpty) {
+      // 画面全体ローディング
+      if (state.isLoading) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
       return const Center(
         child: Text('Github のリポジトリを検索できます'),
       );
@@ -28,22 +30,69 @@ final class RepositoryList extends ConsumerWidget {
     // リポジトリ一覧
     return ListView.separated(
       separatorBuilder: (context, index) => const Divider(),
-      itemCount: state.repositories.length,
+      itemCount: state.itemCount,
       itemBuilder: (context, index) {
-        final repository = state.repositories[index];
+        // リポジトリのインデックスが範囲内であれば、リポジトリセルを返す
+        if (index < state.repositories.length) {
+          final repository = state.repositories[index];
+          return _RepositoryCell(repository);
+        }
+        // それ以外の場合はローディングセルを返す
+        return _LoadingCell(
+          onVisibled: () async {
+            if (!state.hasNextPage) return;
 
-        // リポジトリCell
-        return ListTile(
-          title: Text(repository.fullName),
-          subtitle: Text(repository.description ?? ''),
-          onTap: () async {
-            // 動作: リポジトリ詳細画面に遷移
-            await context.pushNamed(
-              GithubDetailPage.routeName,
-              arguments: repository,
-            );
+            try {
+              //  動作: ページネーション機能
+              await ref
+                  .read(githubSearchNotifierProvider.notifier)
+                  .search(isPaging: true);
+            } catch (e) {
+              debugPrint(e.toString());
+            }
           },
         );
+      },
+    );
+  }
+}
+
+final class _RepositoryCell extends StatelessWidget {
+  const _RepositoryCell(
+    this.repository,
+  );
+
+  final RepositorySchema repository;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(repository.fullName),
+      subtitle: Text(repository.description ?? ''),
+      onTap: () async {
+        // 動作: リポジトリ詳細画面に遷移
+        await context.pushNamed(
+          GithubDetailPage.routeName,
+          arguments: repository,
+        );
+      },
+    );
+  }
+}
+
+final class _LoadingCell extends StatelessWidget {
+  const _LoadingCell({this.onVisibled});
+
+  final Function()? onVisibled;
+  @override
+  Widget build(BuildContext context) {
+    return VisibilityDetector(
+      key: const Key('next_page'),
+      child: const CircularProgressIndicator.adaptive(),
+      onVisibilityChanged: (info) {
+        if (info.visibleFraction > 0.5) {
+          onVisibled?.call();
+        }
       },
     );
   }
--- lib/src/presentation/pages/github_search/github_search_notifier.dart
+++ lib/src/presentation/pages/github_search/github_search_notifier.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:yumemi_flutter_test/src/domain/entity/github/repository/search/query.dart';
 import 'package:yumemi_flutter_test/src/infrastructure/repository/github_repository.dart';
 import 'package:yumemi_flutter_test/src/presentation/pages/github_search/github_search_state.dart';
 
@@ -14,29 +14,38 @@ final class GithubSearchNotifier extends _$GithubSearchNotifier {
 
   GithubRepository get _githubApi => ref.read(githubRepositoryProvider);
 
+  SearchRepositoryQuery get _query => SearchRepositoryQuery(
+        // 検索文言
+        q: state.query,
+        // 現在のページ
+        page: state.page,
+      );
+
   void onQueryChanged(String query) {
     state = state.copyWith(query: query);
   }
 
   /// リポジトリ検索
-  Future<void> search() async {
+  Future<void> search({bool isPaging = false}) async {
     // 空文字の場合は処理しない
     if (state.query.isEmpty) return;
 
     // 既に検索中の場合は処理しない
     if (state.isLoading) return;
 
+    // ページネーション動作であり,次の検索結果がない場合は処理終了
+    if (isPaging && !state.hasNextPage) return;
+
     state = state.copyWith(isLoading: true);
     try {
       // リポジトリ検索
-      final response = await _githubApi.searchRepositories(query: state.query);
+      final response = await _githubApi.searchRepositories(_query);
 
-      debugPrint('リポジトリ数: ${response.totalCount}');
-      debugPrint('${response.items}');
-
-      // リポジトリ一覧を更新
+      // Stateを更新
       state = state.copyWith(
-        repositories: response.items,
+        repositories: [...state.repositories, ...response.items],
+        totalCount: response.totalCount,
+        page: state.page + 1,
       );
     } catch (e) {
       rethrow;
