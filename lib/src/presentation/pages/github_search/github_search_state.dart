e8a2301ff76ab2f83ad3ceab42a977cf44acb773
den0206 <s.yuuki0206@gmail.com>
2024-12-10 21:34:45 +0900

    【機能】ページネーション機能実装

--- lib/src/presentation/pages/github_search/github_search_state.dart
+++ lib/src/presentation/pages/github_search/github_search_state.dart
@@ -9,8 +9,22 @@ class GithubSearchState with _$GithubSearchState {
   const factory GithubSearchState({
     @Default('') String query,
     @Default([]) List<RepositorySchema> repositories,
+    @Default(0) int totalCount,
+    @Default(1) int page,
     @Default(false) bool isLoading,
   }) = _GithubSearchState;
   factory GithubSearchState.fromJson(Map<String, dynamic> json) =>
       _$GithubSearchStateFromJson(json);
+}
+
+extension SearchPageStateExt on GithubSearchState {
+  // 次の検索結果が存在するか？
+  bool get hasNextPage {
+    return totalCount > repositories.length;
+  }
+
+  // ページネーションの為に次の検索結果がある場合はLoading用のCellを表示
+  int get itemCount {
+    return repositories.length + (hasNextPage ? 1 : 0);
+  }
 }
