27345ca1c9ed4b0bc8722e361fe91af97f21000a
den0206 <s.yuuki0206@gmail.com>
2024-12-12 21:11:19 +0900

    【テスト】 SortTypeNotifierTest をAsyncNotifier へ変更

---
+++ test/src/domain/annotations/local_data_source.dart
@@ -0,0 +1,7 @@
+import 'package:mockito/annotations.dart';
+import 'package:yumemi_flutter_test/src/infrastructure/local_data_source/account_local_data_source.dart';
+
+@GenerateNiceMocks([
+  MockSpec<AccountLocalDataSource>(),
+])
+void main() {}
