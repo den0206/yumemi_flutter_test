6c4c617dfa1826ea7dc07536c1d2aab00cc6df19
den0206 <s.yuuki0206@gmail.com>
2024-12-12 18:37:46 +0900

    【機能】ローカルストレージ: AccountLocalDataSource の実装

---
+++ lib/src/core/service/storage_service.dart
@@ -0,0 +1,26 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+// ローカル保存を担当する列挙体
+// 保存したいKeyを列挙体値に追加してください
+// ローカルデータソース 経由で使用してください
+enum StorageService {
+  // 条件検索: ソートタイプ
+  sortType;
+
+  // String 型の保存
+  Future<bool> saveString(String value) async {
+    final pref = await SharedPreferences.getInstance();
+    return pref.setString(name, value);
+  }
+
+  // String 型の読み込み
+  Future<String?> loadString() async {
+    final pref = await SharedPreferences.getInstance();
+    return pref.getString(name);
+  }
+
+  Future<bool> deleteLocal() async {
+    final pref = await SharedPreferences.getInstance();
+    return pref.remove(name);
+  }
+}
