cd824fbcb8005c2b7e4d8279e174fe7e94fdcd01
den0206 <s.yuuki0206@gmail.com>
2024-12-14 12:54:56 +0900

    【統合テスト】 検索シナリオ 実装

---
+++ integration_test/test_data/search_test_data.dart
@@ -0,0 +1,9 @@
+import 'base_test_data.dart';
+
+final class SearchTestData implements BaseTestData {
+  @override
+  String get testQuery => 'openai';
+
+  @override
+  String get testRepositoryName => 'openai/openai-cookbook';
+}
